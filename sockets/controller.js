4adfa17284e1205ddfdabec29032452141aa9c93
br1lisboa <brlisbo@gmail.com>
2022-10-24 17:30:25 -0300

    finalizado modelo de turnos

---
+++ sockets/controller.js
@@ -0,0 +1,24 @@
+const TicketControl = require("../models/ticket-control");
+
+
+const ticketControl = new TicketControl()
+
+const socketController = (socket) => {
+
+    socket.on('enviar-mensaje', ( payload, callback ) => {
+        
+        const id = 123456789;
+        callback( id );
+
+        socket.broadcast.emit('enviar-mensaje', payload );
+
+    })
+
+}
+
+
+
+module.exports = {
+    socketController
+}
+
